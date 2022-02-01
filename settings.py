eb29fdee878a8826537b8ad0e1518597739d7092
Dolinin2021 <programmistdolinin@mail.ru>
2022-02-01 22:54:09 +0600

    Adding a settings.py file

---
+++ settings.py
@@ -0,0 +1,17 @@
+""" Файл настроек. Для дальнейшей работы с проектом необходимо заполнить параметры в этом файле.
+
+SCOPES — это перечень возможностей, которыми будет обладать сервис, созданный в скрипте (Google.Drive).
+Примечание: если вы изменяете область доступа (SCOPES), удалите файл token.json.
+Ссылка на официальную документацию: https://developers.google.com/identity/protocols/oauth2/scopes
+
+yandex_token - ключ доступа (токен), полученный с полигона Яндекса.
+Ссылка на официальную документацию: https://yandex.ru/dev/oauth/doc/dg/concepts/about.html
+
+vk_token - ключ доступа (токен) пользователя Вконтакте.
+Ссылка на официальную документацию: https://dev.vk.com/api/access-token/getting-started
+
+"""
+
+SCOPES = ['']
+yandex_token = ''
+vk_token = ''
